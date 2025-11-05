e77484e9f6588ce405d044e061ee5e50e8b7bf19
Mahdi Ashrafee <ashrafeem8@gmail.com>
2025-11-04 19:52:51 -0500

    chore(functions): add function stubs

---
+++ utils.py
@@ -0,0 +1,14 @@
+def transcribe( audio ):
+    pass 
+
+def voice_response( text ):
+    pass
+
+def youtube_search ( list ):
+    pass
+
+def calender_set ( list ):
+    pass
+
+def qna( list ):
+    pass
